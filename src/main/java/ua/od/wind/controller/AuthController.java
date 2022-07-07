58da69df0737cce75dbad275270430a775fba6e2
Ivan <ivn.ods@gmail.com>
2022-07-07 13:30:10 +0200

    validation ok

--- src/main/java/ua/od/wind/controller/AuthController.java
+++ src/main/java/ua/od/wind/controller/AuthController.java
@@ -3,11 +3,15 @@ package ua.od.wind.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ua.od.wind.model.User;
 import ua.od.wind.security.UserDetailsServiceImpl;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
@@ -25,14 +29,25 @@ public class AuthController {
     }
 
     @GetMapping("/register")
-    public String getRegisterPage() {
+    public String getRegisterPage(@ModelAttribute("userForm") User userForm) {
         return "register";
     }
 
     @PostMapping("/register")
-    public String doRegistration(@ModelAttribute("userForm") User userForm, Model model) {
+    public String doRegistration(@ModelAttribute("userForm")  @Valid User userForm, BindingResult bindingResult) {
 
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())){
+           // bindingResult.addError(new ObjectError("passwordConfirm", "Passwords is different"));
+            bindingResult.rejectValue("passwordConfirm",  "Passwords is different");
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
         userDetailsServiceImpl.saveUser(userForm);
+        // Authenticate
+        //generate pay button
+
         return "ok";
     }
 }
--- src/main/java/ua/od/wind/model/User.java
+++ src/main/java/ua/od/wind/model/User.java
@@ -3,6 +3,8 @@ package ua.od.wind.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -13,14 +15,17 @@ public class User {
     private Long id;
 
     @Column(name = "username")
+    @Size(min=3, message = "Username must be not less than 3 signs")
     private String username;
 
+    @Size(min=6, message = "Password must be not less than 6 signs")
     @Column(name = "password")
     private String password;
 
     @Transient
     private String passwordConfirm;
 
+    @Email(message = "Incorrect email")
     @Column(name = "email")
     private String email;
 
